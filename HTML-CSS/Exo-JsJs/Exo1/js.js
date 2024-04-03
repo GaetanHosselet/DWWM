bcc626b5f53bae3cec18cd94c2cdd4e55a4da2dd
GaetanHosselet <hosselet.gaetan@gmail.com>
2024-04-03 16:26:12 +0200

    03/04/2024

---
+++ HTML-CSS/Exo-JsJs/Exo1/js.js
@@ -0,0 +1,23 @@
+
+// function saluer(prenom) {
+     
+
+//    console.log("Bonjour" + prenom + "" + nom +"!")
+
+
+// }
+
+// Saluer ("thomas", "brandt");
+
+// exo 2 
+
+function multiplication(valeur1, valeur2,){
+    const a= valeur1
+    const b= valeur2
+
+    multiplication=a*b
+}
+
+multiplication (7,8)
+
+console.log(multiplication)
---
+++ HTML-CSS/Exo-JsJs/Exo2/js.js
@@ -0,0 +1,56 @@
+// exo 1/////////////////////////////////////////////////////////////////////////////////
+
+// let age = 18
+
+// if (age <=18) {
+
+//     console.log("mineur")
+// }
+// else {
+//     console.log("majeur")
+// }
+
+// exo 2/////////////////////////////////////////////////////////////////////////////////
+
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i);
+//     }
+
+// exo 3//////////////////////////////////////////////////////////////////////////////////
+
+
+let j = prompt("entre un chiffre entre 1 et 100")
+
+while (j !== "23") {
+    console.log("Noob");
+    j = prompt ("Essaye encore bouffon")
+}
+
+console.log("C'est bien t'as trouvé bouffon. Heureux ?")
+
+// exo 4///////////////////////////////////////////////////////////////////////////////////
+// function nombreJoursDansMois(mois, annee) {
+//     switch (mois) {
+//         case 1: // Janvier
+//         case 3: // Mars
+//         case 5: // Mai
+//         case 7: // Juillet
+//         case 8: // Août
+//         case 10: // Octobre
+//         case 12: // Décembre
+//             return 31;
+//         case 4: // Avril
+//         case 6: // Juin
+//         case 9: // Septembre
+//         case 11: // Novembre
+//             return 30;
+//         case 2: // Février
+//             if ((annee % 4 == 0 && annee % 100 != 0) || annee % 400 == 0) {
+//                 return 29; // Année bissextile
+//             } else {
+//                 return 28; // Année non bissextile
+//             }
+//         default:
+//             return -1; // Valeur invalide pour le mois
+//     }
+// }
