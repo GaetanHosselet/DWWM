cd6911ec9a94fabd58495d9f4f46b6be3fc6adbe
GaetanHosselet <hosselet.gaetan@gmail.com>
2024-04-12 11:52:48 +0200

    12/04/24

--- HTML-CSS/GameProject2/js.js
+++ HTML-CSS/GameProject2/js.js
@@ -1,5 +1,5 @@
 let data = {
-    "Loki" : { "nom" : "Loki", "hp" : 115, "atk" : 10},
+    "Loki" : { "nom" : "Loki", "hp" : 115, "atk" : 10 ,},
     "Thanos" : { "nom" : "Thanos", "hp" : 150, "atk" : 8},
     "Rocket" : { "nom" : "Rocket", "hp" : 85, "atk" : 16},
     "Thor" : { "nom" : "Thor", "hp" : 100, "atk" : 12}
@@ -30,11 +30,13 @@ const P2ATK = document.querySelector ('.P2ATK')
 buttonStart.addEventListener('click', () => {
     selectPerso.style.display = "none";
     combat.style.display = "flex";
+    const pseudoP1 = document.querySelector ('.pseudoJoueur1').value;
+    const pseudoP2 = document.querySelector ('.pseudoJoueur2').value;
 
     if (selectPerso1 === "Loki"){
         imgP1.style.backgroundImage = "url(./img/lokiimg.png)"
         joueur1 = data.Loki
-        nameP1.textContent = joueur1.nom
+        nameP1.textContent = pseudoP1
         P1HP.textContent = joueur1.hp
         P1ATK.textContent = joueur1.atk
 
@@ -42,28 +44,28 @@ buttonStart.addEventListener('click', () => {
     if (selectPerso1 === "Thanos"){
         imgP1.style.backgroundImage = "url(./img/thanosimg.png)"
         joueur1 = data.Thanos
-        nameP1.textContent = joueur1.nom
+        nameP1.textContent = pseudoP1
         P1HP.textContent = joueur1.hp
         P1ATK.textContent = joueur1.atk
     }
     if (selectPerso1 === "Thor"){
         imgP1.style.backgroundImage = "url(./img/thorimg.png)"
         joueur1 = data.Thor
-        nameP1.textContent = joueur1.nom
+        nameP1.textContent = pseudoP1
         P1HP.textContent = joueur1.hp
         P1ATK.textContent = joueur1.atk
     }
     if (selectPerso1 === "Rocket"){
         imgP1.style.backgroundImage = "url(./img/rocketimg.png)"
         joueur1 = data.Rocket
-        nameP1.textContent = joueur1.nom
+        nameP1.textContent = pseudoP1
         P1HP.textContent = joueur1.hp
         P1ATK.textContent = joueur1.atk
     }
     if (selectPerso2 === "Loki"){
         imgP2.style.backgroundImage = "url(./img/lokiimg.png)"
         joueur2 = data.Loki
-        nameP2.textContent = joueur2.nom
+        nameP2.textContent = pseudoP2
         P2HP.textContent = joueur2.hp
         P2ATK.textContent = joueur2.atk
     
@@ -71,21 +73,21 @@ buttonStart.addEventListener('click', () => {
     if (selectPerso2 === "Thanos"){
         imgP2.style.backgroundImage = "url(./img/thanosimg.png)"
         joueur2 = data.Thanos
-        nameP2.textContent = joueur2.nom
+        nameP2.textContent = pseudoP2
         P2HP.textContent = joueur2.hp
         P2ATK.textContent = joueur2.atk
     }
     if (selectPerso2 === "Thor"){
         imgP2.style.backgroundImage = "url(./img/thorimg.png)"
         joueur2 = data.Thor
-        nameP2.textContent = joueur2.nom
+        nameP2.textContent = pseudoP2
         P2HP.textContent = joueur2.hp
         P2ATK.textContent = joueur2.atk
     }
     if (selectPerso2 === "Rocket"){
         imgP2.style.backgroundImage = "url(./img/rocketimg.png)"
         joueur2 = data.Rocket
-        nameP2.textContent = joueur2.nom
+        nameP2.textContent = pseudoP2
         P2HP.textContent = joueur2.hp
         P2ATK.textContent = joueur2.atk
     }
@@ -223,18 +225,49 @@ selectRocketP2.addEventListener('click', () => {
 //  LE COMBAT COMMENCE
 
 const btnAttaqueP1 = document.querySelector ('.btnAttaqueP1')
+const hpP2 = document.querySelector ('.hpP2')
 
 btnAttaqueP1.addEventListener ('click', () => {
     joueur2.hp = joueur2.hp - joueur1.atk
     P2HP.textContent = joueur2.hp
- })
+    hpP2.textContent = joueur2.hp
+    btnAttaqueP1.disabled = true;
+    btnAttaqueP2.disabled = false;
+    btnAttaqueP2.style.borderColor = "Yellow";
+    btnAttaqueP2.style.borderWidth = "7px";
+    btnAttaqueP1.style = "none";
+    
+    if (joueur2.hp <=0) {
+        btnAttaqueP1.disabled = true;
+        btnAttaqueP2.disabled = true;
+        alert ('JOUEUR 1 GAGNE')
+    }
+})
 
 const btnAttaqueP2 = document.querySelector ('.btnAttaqueP2')
+const hpP1 = document.querySelector ('.hpP1')
 
 btnAttaqueP2.addEventListener ('click', () => {
     joueur1.hp = joueur1.hp - joueur2.atk
     P1HP.textContent = joueur1.hp
+    hpP1.textContent = joueur1.hp
+    btnAttaqueP2.disabled = true;
+    btnAttaqueP1.disabled = false;
+    btnAttaqueP1.style.borderColor = "Yellow";
+    btnAttaqueP1.style.borderWidth = "7px";
+    btnAttaqueP2.style = "none";
+
+    
+
+    if (joueur1.hp <=0) {
+        btnAttaqueP1.disabled = true;
+        btnAttaqueP2.disabled = true;
+        alert ('JOUEUR 2 GAGNE')
+    }
 })
+
+
+
 
 
 
